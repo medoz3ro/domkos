e28cfc970c978a28a3c004d73cf69afb68059760
medoz3ro <bsabo44@gmail.com>
2024-11-07 23:25:56 +0100

    Chat messaging
    Chat messages on submit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { getFirestore, collection, query, orderBy, limit } from 'firebase/firestore';
+import { getFirestore, collection, query, orderBy, limit, serverTimestamp, addDoc } from 'firebase/firestore';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState, useRef } from 'react';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAvy0w851FbcVUXZX51JTLqpPGpXOVMHZ8",
@@ -22,11 +23,13 @@ const auth = getAuth(app);
 const firestore = getFirestore(app);
 
 function App() {
-  const [user] = useAuthState(auth); // Use it only within this component
+  const [user] = useAuthState(auth);
 
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <header>
+        {/* You can place your header content here */}
+      </header>
       <section>
         {user ? <Chat /> : <SignIn />}
       </section>
@@ -51,18 +54,54 @@ function SignOut() {
 
 function ChatMessage(props) {
   const { text, uid } = props.message;
-  return <p>{text}</p>;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={props.message.photoURL} alt="User" />
+      <p>{text}</p>
+    </div>
+  );
 }
 
 function Chat() {
+
+const dummy = useRef()
+
   const messageReferences = collection(firestore, 'poruke');
   const messagesQuery = query(messageReferences, orderBy('createdAt'), limit(25));
   const [messages] = useCollectionData(messagesQuery, { idField: 'id' });
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await addDoc(messageReferences, {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+
+    dummy.current.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <div>
-      {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-      <SignOut />
+      <main>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button type='submit'>Pošalji</button>
+      </form>
     </div>
   );
 }
