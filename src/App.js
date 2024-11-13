bbcc7e4722df804946fb169049a6220c7eb6daa7
medoz3ro <bsabo44@gmail.com>
2024-11-13 22:34:00 +0100

    fix: code
    .env

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ import { useEffect, useState, useRef } from "react";
 
 // Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyAvy0w851FbcVUXZX51JTLqpPGpXOVMHZ8",
-  authDomain: "domko-cbe7c.firebaseapp.com",
-  projectId: "domko-cbe7c",
-  storageBucket: "domko-cbe7c.appspot.com",
-  messagingSenderId: "637063359036",
-  appId: "1:637063359036:web:d087dcb8b8156cbc40b139",
-  measurementId: "G-X4C19X83YL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app and services
